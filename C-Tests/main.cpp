dc591c799c50356ad78480c4474e5ab46a0cdedd
vakanto <>
2018-03-20 20:49:22 +0100

    Initialisierung

---
+++ C-Tests/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: vakanto
+ *
+ * Created on 20. März 2018, 20:48
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
